2cefdb12307ca6eaf478f5e91118818de57c41dd
pedroaurelli <pedroaureliano.contato@gmail.com>
2026-02-12 03:45:18 -0300

    feat: add validateEmpresaAtualMiddleware to various routers for company validation

--- apps/backend-node/src/routers/authRouter.ts
+++ apps/backend-node/src/routers/authRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { generateToken } from '../lib/jwt'
 import { loginService } from '../services/authentication/loginService'
-import { getAuthenticatedUser } from '../middleware/authMiddleware'
+import { getAuthenticatedUser, authMiddleware } from '../middleware/authMiddleware'
 import { resetUsuarioSenhaCommand } from '../services/authentication/resetUsuarioSenhaService'
+import { selectEmpresa } from '../services/authentication/selectEmpresa'
 
 export const authRouter = Router();
 
@@ -16,6 +17,15 @@ authRouter.post("/login", async (req, res) => {
   return res.json(user);
 });
 
+authRouter.post("/select-empresa", authMiddleware, async (req, res) => {
+  const result = await selectEmpresa(req.user, req.body);
+
+  const token = generateToken(result);
+
+  res.cookie("token", token, { httpOnly: true, maxAge: 86400000 });
+
+  return res.json(result);
+});
 
 authRouter.post("/logout", (req, res) => {
   res.clearCookie("token");
