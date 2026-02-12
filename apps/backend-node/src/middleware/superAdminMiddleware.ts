098acfaf3f30a095a57904353315ffcc144a82f5
pedroaurelli <pedroaureliano.contato@gmail.com>
2026-02-12 11:57:56 -0300

    fix: update user permission checks in middleware to use optional chaining

--- apps/backend-node/src/middleware/superAdminMiddleware.ts
+++ apps/backend-node/src/middleware/superAdminMiddleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function superAdminMiddleware(req: Request, res: Response, next: NextFunction) {
-  const userPermission = req.user.is_superadmin
+  const userPermission = req.user?.isSuperAdmin
 
   if (!userPermission) {
     return res.status(403).json({ message: "Acesso negado" })
--- apps/backend-node/src/middleware/validateEmpresaAtualMiddleware.ts
+++ apps/backend-node/src/middleware/validateEmpresaAtualMiddleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function validateEmpresaAtualMiddleware(req: Request, res: Response, next: NextFunction) {
-  if (!req.user.empresa) {
+  if (!req.user?.empresa) {
     return res.status(403).json({ message: "Empresa não selecionada. Por favor, selecione uma empresa para continuar." })
   }
 
